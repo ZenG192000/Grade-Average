e9074476b9c7b85a616596820e4b53d2785ba792
Joven R. Almonte <Joven R. Almonte@SERVER>
2022-11-06 15:25:31 +0800

    Add project files.

---
+++ Grade Average/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+double number1, number2, number3, number4;
+
+Console.Write("Enter the Prelim Grade: ");
+number1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Enter the Midterm Grade: ");
+number2 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Enter the PreFinal Grade: ");
+number3 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Enter the Final Grade: ");
+number4 = Convert.ToDouble(Console.ReadLine());
+
+double result = (number1 + number2 + number3 + number4) / 4;
+Console.WriteLine("Your average is " + result);
